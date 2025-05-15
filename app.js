688e8658895e99be2ffd90e8b964b6d96ff7ce45
MUHAMMAD ATTAULLAH KHAN GISHKORI <156325673+MUHAMMMAD-ATTAULLAH-KHAN-GISHKORI@users.noreply.github.com>
2025-05-16 01:36:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const chatRoutes = require("./routes/chatRoutes");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { globalErrorHandler, notFound } = require("./middleware/errorHandler");
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.options("*", cors());
+app.get("/", (req, res) => {
+  res.send("Welcome to Chat Application!");
+});
+
+app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/message", messageRoutes);
+
+//  For Invalid Routes
+app.use(notFound);
+//  For Global Error Handler
+app.use(globalErrorHandler);
+module.exports = app;
